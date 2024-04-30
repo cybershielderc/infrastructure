5db08f66efaef78ecbd7f09449902aec2db9a13f
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 09:53:27 +0300

    Commit H-703475e541514fe3

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -21,4 +21,4 @@ class NexVerse:
         self.app = self._initialize_bot()
 
     def _initialize_bot(self) -> ApplicationBuilder:
-        app = ApplicationBuilder
+        app = ApplicationBuilder()
