280b4ad7ca7054ec79a13a7551b827fdf721f200
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 13:21:32 +0300

    Commit H-911e6d5877d52c08

--- nexverse/bot/image_models_menu.py
+++ nexverse/bot/image_models_menu.py
@@ -7,6 +7,9 @@ from telegram import (
 def ai_models_image_menu() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         [
-            
+            [InlineKeyboardButton("Realism", callback_data="ignore_0xdead")],
+            [
+                
+            ]
         ]
     )
