0ab532ae373c4500a1b1c5930a3eb5d24bcc9a28
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 17:38:44 +0300

    Commit H-1abb6369ac326fa0

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -24,7 +24,7 @@ class NexVerse:
 
     def _initialize_bot(self) -> ApplicationBuilder:
         app: ApplicationBuilder = ApplicationBuilder().token(self.token).build()
-        #app.add_handler(CallbackQueryHandler(self.button_handler))
+        # app.add_handler(CallbackQueryHandler(self.button_handler))
         app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.text_input))
         app.add_handler(CommandHandler("start", self.start_menu))
         return app
