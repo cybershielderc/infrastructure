84481301d0950ac195492e6de272ab76fa0c1369
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 22:17:35 +0300

    Commit H-8d1d457b3543e274

--- nexverse/bot/start_menu.py
+++ nexverse/bot/start_menu.py
@@ -14,7 +14,7 @@ def start_menu_markup() -> InlineKeyboardMarkup:
             [
                 InlineKeyboardButton("Text-to-Image (TTI)", callback_data="text_to_image"),
                 InlineKeyboardButton("Image-to-Image (WIP)", callback_data="ignore_0xdead"),  # "image_to_image"),
-                InlineKeyboardButton("AI Assistant", callback_data="initiate_chat"),
+                InlineKeyboardButton("AI Assistant", callback_data="ignore_0xdead"),  # "initiate_chat"),
             ]
         ]
     )
