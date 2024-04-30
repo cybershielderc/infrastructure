8349051639c1cc4a7f6f8a449124860bfc5fcbb3
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 13:26:49 +0300

    Commit H-17e7ff6b50b2cae8

--- nexverse/bot/image_models_menu.py
+++ nexverse/bot/image_models_menu.py
@@ -11,7 +11,7 @@ def ai_models_image_menu() -> InlineKeyboardMarkup:
             [
                 InlineKeyboardButton("MidJourney v4", callback_data="text_midjourney"),
                 InlineKeyboardButton("Realistic Vision v4", callback_data="text_rvision"),
-                InlineKeyboardButton("", callback_data="")
+                InlineKeyboardButton("Juggernaut XL", callback_data="text_juggernaut")
             ]
         ]
     )
