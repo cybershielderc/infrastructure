478ce0331bd4859ea4ba2203dbe3727740a1b7fb
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 17:33:59 +0300

    Commit H-870d178be51a8736

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
     # Initiate Eth Net Connection
     cryptocurrency.initiate_eth_net()
     tele_app = run_app(token, cryptocurrency)
-    nexverse = nexverse_runbot()
+    nexverse = nexverse_runbot(data['nexverse']['token'])
     print("Running")
