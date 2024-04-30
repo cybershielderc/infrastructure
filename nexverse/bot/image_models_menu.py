ce388bbbdd1435877f239e0fec0be66801e3b974
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 13:21:45 +0300

    Commit H-c8e128d877c03d10

--- nexverse/bot/image_models_menu.py
+++ nexverse/bot/image_models_menu.py
@@ -9,7 +9,7 @@ def ai_models_image_menu() -> InlineKeyboardMarkup:
         [
             [InlineKeyboardButton("Realism", callback_data="ignore_0xdead")],
             [
-                InlineKeyboardButton("MidJourney v4")
+                InlineKeyboardButton("MidJourney v4", callback_data="ignore_0xdead")
             ]
         ]
     )
