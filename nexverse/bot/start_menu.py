316f4e34cb0010674b24109ecd817c22ca0b0763
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 10:42:55 +0300

    Commit H-0e121479d7c12469

--- nexverse/bot/start_menu.py
+++ nexverse/bot/start_menu.py
@@ -13,7 +13,7 @@ def start_menu_markup() -> InlineKeyboardMarkup:
             [InlineKeyboardButton("🛠️ Services & Invoices 🧾", callback_data="ignore_0xdead")],
             [
                 InlineKeyboardButton("Text-to-Image (TTI)", callback_data="text_to_image"),
-                InlineKeyboardButton("Image-to-Image (ITI)", callback_data="image_")
+                InlineKeyboardButton("Image-to-Image (ITI)", callback_data="image_to")
             ]
         ]
     )
