e5d20b47b2c90a5e818ef86051f1ad5e556a80ac
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 08:57:25 +0300

    Created __init__.py

---
+++ nexverse/bot/__init__.py
@@ -0,0 +1,21 @@
+import asyncio
+from telegram import (
+    Update, InlineKeyboardButton, InlineKeyboardMarkup
+)
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    CallbackContext,
+    ContextTypes,
+    filters
+)
+
+import re
+import time
+
+
+class NexVerse:
+    def __init__(self, token: str):
+        self.token = token
+        self.app = self._initialize_bot()
