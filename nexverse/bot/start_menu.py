986c0268bf34105afedc1b0e70682d142991073f
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 10:40:59 +0300

    Commit H-61000a5ab09628dd

--- nexverse/bot/start_menu.py
+++ nexverse/bot/start_menu.py
@@ -6,7 +6,9 @@ def start_menu_markup() -> InlineKeyboardMarkup:
         [
             [InlineKeyboardButton("💸 Wallet & Balance 💸", callback_data="ignore_0xdead")],
             [
-
+                InlineKeyboardButton("Connect Wallet", callback_data="wallet_connect"),
+                InlineKeyboardButton("$CSL Balance (WIP)", callback_data="csl_balance"),
+                InlineKeyboardButton("My Wallet", callback_data="display_wallet_address")
             ]
         ]
     )
