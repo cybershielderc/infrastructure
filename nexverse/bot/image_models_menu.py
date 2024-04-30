bf96e7c81b6ed9dae0052278a32a28574ff1f114
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 13:36:46 +0300

    Commit H-a48117005a9ab16b

--- nexverse/bot/image_models_menu.py
+++ nexverse/bot/image_models_menu.py
@@ -16,7 +16,7 @@ def ai_models_image_menu() -> InlineKeyboardMarkup:
             [InlineKeyboardButton("Anime", callback_data="ignore_0xdead")],
             [
                 InlineKeyboardButton("Anything v4", callback_data="text_anything"),
-                InlineKeyboardButton("Dark Sushi 2.5D", callback_data="text_"),
+                InlineKeyboardButton("Dark Sushi 2.5D", callback_data="text_dark"),
                 InlineKeyboardButton(),
             ]
         ]
