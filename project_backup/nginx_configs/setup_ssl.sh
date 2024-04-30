0c6bc097bdae8a571577e5fdbe8ec11f0cb38136
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 09:00:07 +0300

    Created setup_ssl.sh

---
+++ project_backup/nginx_configs/setup_ssl.sh
@@ -0,0 +1,20 @@
+certbot certonly --webroot -d api.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d as-vpn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d cdn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d db.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d eu-vpn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d in-vpn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d mail.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d me-vpn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d na-vpn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d oc-vpn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d panel.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d sa-vpn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d us-vpn.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d smtp.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+certbot certonly --webroot -d node1.csl.sh --email talbaskin.business@gmail.com -w /var/www/_letsencrypt -n --agree-tos --force-renewal
+
+sed -i -r 's/#?;#//g' /etc/nginx/sites-available/*
+
+
