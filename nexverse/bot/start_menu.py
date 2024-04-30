e7fbd825e14bcd3642518aced2d60d6e9c464273
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-04-30 10:40:56 +0300

    Commit H-d0a6ad2f4b2e596b

--- nexverse/bot/start_menu.py
+++ nexverse/bot/start_menu.py
@@ -5,6 +5,8 @@ def start_menu_markup() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("💸 Wallet & Balance 💸", callback_data="ignore_0xdead")],
-            
+            [
+
+            ]
         ]
     )
