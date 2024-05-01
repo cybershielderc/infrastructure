ada4f0e2b7897fb1d15a271a20c0520addc5bfb6
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 20:22:51 +0300

    Commit H-92b4c537907718b7

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -89,8 +89,6 @@ class TextToImage:
 
 api = TextToImage(data['apis']['modelslab'])
 negative_prompt = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature"
-nsfw_prompts = [
-    "hot lady in leggings, and see-through bra",
-]
+nsfw_prompts = "hot lady in leggings, and see-through bra"
 response: requests.Request = api.build_request(MODEL.PINKDREAM, "hot lady in leggings, and see-through bra",
                                                "misfigured face")
