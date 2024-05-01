f051be89159c1f7c7ed6731162511888207ce36b
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 22:37:03 +0300

    Commit H-9db8d48d8a09291d

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -96,10 +96,11 @@ class NexVerse:
             await update.message.reply_text(
                 text="Sorry, I couldn't generate that image for you! :/\nPlease try again!."
             )
-        print(image)
+
         if image['status'] is 'processing':
             await asyncio.sleep(image['eta'] + 0.5)
             image['output'][0] = self.ai_image_api.get_queued(image['id'])
+        print(image)
         print(self.ai_image_api.get_model(context.user_data['selected_model']))
         print(context.user_data['pos_prompt'])
         message = f"Successfully generated!\n<strong>Image ID</strong> <code>{image['id']}</code>\n" + \
