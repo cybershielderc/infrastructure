28b41579a9a29842c6c4ea9c0ce0dc9d3edd3522
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 21:30:46 +0300

    Commit H-391e74cf09aa42a9

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -83,7 +83,7 @@ class NexVerse:
                     # Capture message
                     user_input = update.message.text
                     await update.message.delete()
-                    await update.message.re(f'You entered: {user_input}')
+                    await update.message.reply_to_message.(f'You entered: {user_input}')
                     context.user_data['waiting_for_prompt'] = False
 
     def run(self):
