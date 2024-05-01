a50b69e18fbf296b901a6b32deaddd8c415236ac
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 23:08:32 +0300

    Commit H-920ee0e696d84645

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -121,7 +121,8 @@ class NexVerse:
             try:
                 image['output'][0] = self.ai_image_api.get_queued(image['id'])
             except IndexError:
-                await asyncio.sleep(image['eta'] * 2)
+                await asyncio.sleep(image['eta'] + 2)
+                image['output'][0] = self.ai_image_api.get_queued(image['id'])
             print(image)
         print(self.ai_image_api.get_model(context.user_data['selected_model']))
         print(context.user_data['pos_prompt'])
