42dcb231e81a78ab3efb6a348d01fbecd34a91d3
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 20:34:45 +0300

    Commit H-245bdb0bca0b0332

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -106,6 +106,7 @@ for k, v in api.MODELS:
 i = 0
 for i in range(len(responses)):
     if 0 <= i <= 2:
+        
         print(f"NSFW Response-{i}: {responses[i].json['output'][0]}")
    if 3 <= i <= 6:
         print(f"Anime Response-{i}: {responses[i]}")
