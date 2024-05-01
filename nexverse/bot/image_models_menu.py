9ddb56055a919d363a59a03b2151ac3965923393
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 21:22:02 +0300

    Commit H-da0039acb4594a3a

--- nexverse/bot/image_models_menu.py
+++ nexverse/bot/image_models_menu.py
@@ -9,21 +9,26 @@ def ai_models_image_menu() -> InlineKeyboardMarkup:
         [
             [InlineKeyboardButton("Realism", callback_data="ignore_0xdead")],
             [
-                InlineKeyboardButton("MidJourney v4", callback_data="ai>img>model>selection#//realism1"),
-                InlineKeyboardButton("Realistic Vision v4", callback_data="ai>img>model>selection#//realism2"),
-                InlineKeyboardButton("Juggernaut XL", callback_data="ai>img>model>selection#//realism3")
+                InlineKeyboardButton("MidJourney v4",
+                                     callback_data="ai>img>model>selection#//realism1#CNAME#MidJourney V4"),
+                InlineKeyboardButton("Realistic Vision v4",
+                                     callback_data="ai>img>model>selection#//realism2#CNAME#Realistic Vision V4"),
+                InlineKeyboardButton("Juggernaut XL",
+                                     callback_data="ai>img>model>selection#//realism3#CNAME#Juggernaut XL")
             ],
             [InlineKeyboardButton("Anime", callback_data="ignore_0xdead")],
             [
-                InlineKeyboardButton("Anything v4", callback_data="ai>img>model>selection#//anime1"),
-                InlineKeyboardButton("Dark Sushi 2.5D", callback_data="ai>img>model>selection#//anime2"),
-                InlineKeyboardButton("Sakura v3", callback_data="ai>img>model>selection#//anime3")
+                InlineKeyboardButton("Anything v4", callback_data="ai>img>model>selection#//anime1#CNAME#Anything V4"),
+                InlineKeyboardButton("Dark Sushi 2.5D",
+                                     callback_data="ai>img>model>selection#//anime2#CNAME#Dark Sushi 2.5D"),
+                InlineKeyboardButton("Sakura v3", callback_data="ai>img>model>selection#//anime3#CNAME#Sakura v3")
             ],
             [InlineKeyboardButton("NSFW", callback_data="ignore_0xdead")],
             [
-                InlineKeyboardButton("Deliberate", callback_data="ai>img>model>selection#//nsfw1"),
-                InlineKeyboardButton("Perfect Deli", callback_data="ai>img>model>selection#//nsfw2"),
-                InlineKeyboardButton("Mix Appfactory", callback_data="ai>img>model>selection#//nsfw3"),
+                InlineKeyboardButton("Deliberate", callback_data="ai>img>model>selection#//nsfw1#CNAME#Deliberate"),
+                InlineKeyboardButton("Perfect Deli", callback_data="ai>img>model>selection#//nsfw2#CNAME#Perfect Deli"),
+                InlineKeyboardButton("Mix Appfactory",
+                                     callback_data="ai>img>model>selection#//nsfw3#CNAME#Mix Appfactory"),
                 InlineKeyboardButton("Dark Appfactory (Anime)",
                                      callback_data="ai>img>model>selection#//nsfw4")
             ],
