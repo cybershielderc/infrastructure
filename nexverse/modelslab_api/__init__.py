b047fed761f316499e1bec2ac5ca964511cb235c
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 18:14:22 +0300

    Commit H-fd2957584bf66560

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -1,3 +1,4 @@
+import enum
 import requests
 import json
 
@@ -11,13 +12,15 @@ api_data = data['apis']['modelslab']
 class URIS:
     TTI = "https://modelslab.com/api/v6/images/text2img"
 
+class MODELS(enum.Enum):
+    PINKDREAM = ""
 
 class TextToImage:
     def __init__(self, api: str = None) -> None:
         if not api: raise Exception("No api key provided")
         self.api_key = api
 
-    def build_request(self, model):
+    def build_request(self, model:):
         return json.dumps({
             "key": self.api_key,
             "model_id":
