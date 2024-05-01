67a91fb93d3ccbf85a3a05829e55743f6855f652
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 23:13:30 +0300

    Commit H-c5be55f9b1963877

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -147,7 +147,7 @@ class NexVerse:
                     await update._bot.delete_message(chat_id=update.message.chat_id,
                                                      message_id=context.user_data['tti_response_message'])
                     context.user_data['tti_response_message'] = None
-            
+            image_bytes: bytes = get_file_byte_data_from_url(image['output'][0])
             response_message = await update.message.reply_photo(
                 photo=,
                 caption=message,
