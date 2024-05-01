208284e4eaf756c7480612aa8f889b109ff1ebfc
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 21:53:06 +0300

    Commit H-c4b7da5861d6c318

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -39,7 +39,7 @@ class TextToImage:
     def build_request(self,
                       model: MODEL,
                       prompt: str = None,
-                      negative_prompt: str = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature",
+                      negative_prompt
                       size: [int, int] = [512, 512],
                       samples: int = 1,
                       num_inference_steps: int = 30,
