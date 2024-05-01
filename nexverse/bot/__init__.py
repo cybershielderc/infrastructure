8aba901bd3c83cc3ad242da8112b655914320591
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 23:00:11 +0300

    Commit H-48ebb0a73f7b5830

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -129,7 +129,7 @@ class NexVerse:
         try:
             if 'tti_response_message' in context.user_data:
                 if context.user_data['tti_response_message'] is not None:
-                    await update._bot.delete_message()
+                    await update._bot.delete_message(m)
             response_message = await update.message.reply_photo(
                 photo=get_file_byte_data_from_url(image['output'][0]),
                 caption=message,
