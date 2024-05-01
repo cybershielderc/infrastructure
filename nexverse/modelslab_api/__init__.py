74c363bb26aefdf96e0b646e4d22cb7a9fe8d5be
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 20:43:50 +0300

    Commit H-59b5711d753d7eb9

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -85,8 +85,3 @@ class TextToImage:
         request = requests.request("POST", URIS.TTI, headers=header, data=payload)
 
         return request
-
-    def get_model(self, model_id: str) -> MODEL:
-        return {
-            ""
-        }
