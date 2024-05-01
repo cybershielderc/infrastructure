439f87b005c843cab44079ca3d5983d89970847a
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 22:49:19 +0300

    Commit H-c5fb4c1226eb4e42

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -113,7 +113,7 @@ class NexVerse:
 
         if not 'generationTime' in image.keys():
             await asyncio.sleep(image['eta'] + 0.5)
-            image['output'][0] = self.ai_image_api.get_queued(image['id'])
+            image['output'] = self.ai_image_api.get_queued(image['id'])
         print(image)
         print(self.ai_image_api.get_model(context.user_data['selected_model']))
         print(context.user_data['pos_prompt'])
