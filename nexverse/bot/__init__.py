87692cd27a9167dfe8a716b5d13bbdefd72e49ba
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-02 00:00:02 +0300

    Commit H-f244ddc73eedc1c2

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -121,8 +121,9 @@ class NexVerse:
             time.sleep(image['eta'] + 1)
             print(image)
             try:
-                image['output'][0] = 
-            image['output'][0] = image_url
+                image['output'][0] = image['output'][0]
+            except IndexError:
+                
         message = f"Successfully generated!\n<strong>Image ID</strong> <code>{image['id']}</code>\n" + \
                   f"<strong>Time Took {image['generationTime']:.2f} seconds</strong>\n\n<strong>Prompt</strong>\n" + \
                   f"{context.user_data['pos_prompt']}\n\n<strong>Negative Prompt</strong>\n{context.user_data['neg_prompt']}" + \
