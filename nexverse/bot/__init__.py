b65625d5cb2a12ae0d1dbf9cfd76987c09119b51
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 21:27:19 +0300

    Commit H-ca1ca9c1eb848d98

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -64,7 +64,8 @@ class NexVerse:
             selection = query.data.split("ai>img>model>selection#//")[1].split("#CNAME#")
             if update.message is None:
                 await update.callback_query.edit_message_text(
-                    f"You have selected <strong>{selection[1]}</strong><br>Please reply to this message to set the prompt you would like the AI to use!"
+                    f"You have selected <strong>{selection[1]}</strong><br>Please reply to this message to set the prompt you would like the AI to use!",
+                    "HTML",
                     
                 )
             else:
