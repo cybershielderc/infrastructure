77655fa65392b043fde8958f06592d2335da852c
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 18:28:24 +0300

    Commit H-53f64fc2d662fe2c

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -31,7 +31,7 @@ class TextToImage:
                       num_inference_steps: int = 30,
                       seed: int = None,
                       guidance_scale: float = 7.5
-                      ) -> requests.api.:
+                      ) -> requests.api:
         if not prompt or not negative_prompt: raise Exception("No prompt/negative prompt provided")
         payload = json.dumps({
             "key": self.api_key,
@@ -60,4 +60,4 @@ api = TextToImage(data['apis']['modelslab'])
 response = api.build_request(MODEL.PINKDREAM, "hot lady in leggings, and see-through bra",
                              "painting, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, deformed, ugly, blurry, bad anatomy, bad proportions, extra limbs, cloned face, skinny, glitchy, double torso, extra arms, extra hands, mangled fingers, missing lips, ugly face, distorted face, extra legs, anime")
 
-print(response)
+print(response.text)
