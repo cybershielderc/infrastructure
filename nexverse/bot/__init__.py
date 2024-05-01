212daa3637788ca3f2e0233660b4e09d103fe070
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 22:49:52 +0300

    Commit H-58200b59ccb9d5e3

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -112,9 +112,10 @@ class NexVerse:
             )
 
         if not 'generationTime' in image.keys():
-            await asyncio.sleep(image['eta'] + 0.5)
-            image['output_url'] = self.ai_image_api.get_queued(image['id'])
-        
+            await asyncio.sleep(image['eta'] + 0.5
+            print(image)
+            image['output'][0] = self.ai_image_api.get_queued(image['id'])
+        print(image)
         print(self.ai_image_api.get_model(context.user_data['selected_model']))
         print(context.user_data['pos_prompt'])
         message = f"Successfully generated!\n<strong>Image ID</strong> <code>{image['id']}</code>\n" + \
