7e814b0de727ccf8bad5b7b9514be31f542dd2c5
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 18:19:47 +0300

    Commit H-d61d7b9fd1e3fee3

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -20,7 +20,7 @@ class TextToImage:
         if not api: raise Exception("No api key provided")
         self.api_key = api
 
-    def build_request(self, model: MODELS):
+    def build_request(self, model: MODEL, prompt: str):
         return json.dumps({
             "key": self.api_key,
             "model_id":
