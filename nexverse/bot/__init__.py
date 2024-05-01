255e356cbb098e6d520fbf104860176b094bb089
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 21:58:59 +0300

    Commit H-d9b746ef187d113b

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -87,7 +87,7 @@ class NexVerse:
                                                    'neg_prompt'] is not '-' else self.ai_image_api.DEFAULT_NEG_PROMPT
         ).json()
         message = f"Successfully generated!\n<strong>Image ID</strong> <code>{image['id']}</code>" + \
-                  ""
+                  "<strong>"
 
     async def text_input(self, update: Update, context: CallbackContext):
         if 'waiting_for_neg_prompt' in context.user_data:
