48883bf5d7f4c73abce5d7b66de31ec57e646ee2
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 22:56:18 +0300

    Commit H-3801dcbc50ef6d72

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -115,6 +115,7 @@ class NexVerse:
             await asyncio.sleep(image['eta'] + 1)
             print(image)
             image['output'][0] = self.ai_image_api.get_queued(image['id'])
+            print(image)
         print(self.ai_image_api.get_model(context.user_data['selected_model']))
         print(context.user_data['pos_prompt'])
         message = f"Successfully generated!\n<strong>Image ID</strong> <code>{image['id']}</code>\n" + \
