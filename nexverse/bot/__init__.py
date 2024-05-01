c46cce5385ffdee42c08ee168507a2d27765e346
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 22:38:45 +0300

    Commit H-ace20c663d4c9457

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -21,6 +21,18 @@ from ..modelslab_api import MODEL, TextToImage
 import re
 import time
 
+def get_file_byte_data_from_url(file_url):
+    try:
+        response = requests.get(file_url)
+        # Check if the request was successful
+        if response.status_code == 200:
+            return response.content  # Return the byte data
+        else:
+            print(f"Failed to retrieve file from URL: {file_url}. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred while retrieving file from URL: {file_url}. Error: {str(e)}")
+        return None
 
 class NexVerse:
     def __init__(self, token: str, ai_image_api: TextToImage):
