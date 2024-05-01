e138e742b787282bb418053bd2813623853283af
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 21:18:08 +0300

    Commit H-2ff3277ffa090f74

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -68,7 +68,7 @@ class NexVerse:
 
     async def text_input(self, update: Update, context: CallbackContext):
         if 'waiting_for_prompt' in context.user_data:
-            if context.user_data['waiting_for_input']:
+            if context.user_data['waiting_for_prompt']:
                 user_input = update.message.text
                 update.message.reply_text(f'You entered: {user_input}')
                 context.user_data['waiting_for_input'] = False
