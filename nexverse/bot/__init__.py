9d007791fc5ce5ffdbcb111baa4bbf5a4ff4b798
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 23:25:50 +0300

    Commit H-8124319e6885ee62

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -81,7 +81,6 @@ class NexVerse:
         await query.answer()
         if query.data == "regenerate_data":
             await update._bot.send_message(text="Regenerating... ", chat_id=query.message.chat_id,
-                                           message_id=context.user_data["tti_response_message"])
             await self.reply_with_generated_image(update, context)
         if query.data.startswith('ai>img>model>selection#//'):
             selection = query.data.split("ai>img>model>selection#//")[1].split("#CNAME#")
