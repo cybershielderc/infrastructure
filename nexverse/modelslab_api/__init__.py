ac60f7027083db9d05635b03bc46f5826813f71f
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-01 22:07:04 +0300

    Commit H-66a79116349b0b99

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -2,12 +2,6 @@ import enum
 import requests
 import json
 
-with open("../cfgs/bot.json", "r") as file:
-    data = json.loads(file.read())
-    file.close()
-
-api_data = data['apis']['modelslab']
-
 
 class URIS:
     TTI = "https://modelslab.com/api/v6/images/text2img"
