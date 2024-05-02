e8902999b43ed6b5664bfae7fe22d062bca21380
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-02 13:27:33 +0300

    Commit H-4462a74c227ab53a

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -63,7 +63,7 @@ class TextToImage:
         print(model, prompt, negative_prompt[0])
         request = requests.request("POST", URIS.TTI, headers=header, data=payload)
         request_response: requests.Response = request.raise_for_status()
-        
+        if request_response.js
     def get_model(self, model_id: str) -> MODEL:
         return {
             # NSFW
