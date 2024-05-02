77fe48356474352a19a4b91fda9817dace28813a
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-02 13:32:25 +0300

    Commit H-329e94459f0dfe8f

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -63,6 +63,7 @@ class TextToImage:
         request = requests.request("POST", URIS.TTI, headers=header, data=payload)
         try:
             req_response: requests.Response = request.raise_for_status()
+        except requests.exceptions.HTTPError:
             
         request_response = request.raise_for_status()
         while (request_response.json()['status'] != 'success'):
