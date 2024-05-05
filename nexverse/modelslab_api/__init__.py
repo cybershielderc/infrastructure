c3e33af7c9e991ca7144e77482cb6d83cbfe27d1
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-05 19:29:47 +0300

    Commit H-f725c142367a91c8

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -24,7 +24,8 @@ class MODEL(enum.Enum):
     DARK_SUSHI = "dark-sushi-25d"
     SAKURA = "sakurav3"
 
-class TextToImageAsync
+class TextToImageAsynchronous:
+    
 
 class TextToImage:
     DEFAULT_NEG_PROMPT: str = "lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature",
