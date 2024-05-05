181ea3b8e5a92185a3ded8da4dbea28d6f5e57fb
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-05 19:30:47 +0300

    Commit H-d25fab6bfe30567e

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -104,4 +104,40 @@ class TextToImageAsynchronous(TextToImage):
         if not api: raise Exception("No api key provided")
         self.api_key = api
 
-    async def get_
+    async def get_image(self):
+        def build_request(self,
+                          model: MODEL,
+                          prompt: str = None,
+                          negative_prompt: str = DEFAULT_NEG_PROMPT[0],
+                          size: [int, int] = [512, 512],
+                          samples: int = 1,
+                          num_inference_steps: int = 30,
+                          seed: int = None,
+                          guidance_scale: float = 7.5
+                          ) -> requests.api:
+            if not prompt or not negative_prompt: raise Exception("No prompt/negative prompt provided")
+            payload = json.dumps({
+                "key": self.api_key,
+                "model_id": model.value,
+                "prompt": prompt,
+                "negative_prompt": negative_prompt[0] if type(negative_prompt) == tuple else negative_prompt,
+                "width": size[0],
+                "height": size[1],
+                "samples": samples,
+                "num_inference_steps": num_inference_steps,
+                "seed": seed,
+                "guidance_scale": guidance_scale,
+                "scheduler": "UniPCMultistepScheduler",
+                "webhook": None,
+                "track_id": None,
+            })
+            header = {
+                'Content-Type': 'application/json'
+            }
+            request = requests.request("POST", URIS.TTI, headers=header, data=payload)
+
+            try:
+                response: requests.Response = request.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                print(f"HTTP Error: {e}")
+            return request
