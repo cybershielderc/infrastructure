3bf204a5154c54635cd99c81920132c26f4f9830
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-05 19:36:02 +0300

    Commit H-fa970752af7526db

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -108,7 +108,7 @@ class TextToImageAsynchronous(TextToImage):
         if not api: raise Exception("No api key provided")
         self.api_key = api
 
-    async def build_request(self,
+    async def fetch_image(self,
                             model: MODEL,
                             requesting_uid: int = None,
                             prompt: str = None,
