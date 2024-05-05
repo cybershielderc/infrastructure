155c886972b32f971d93e6141e710e25b810b23e
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-05 20:29:35 +0300

    Commit H-85a6f68ae86b5118

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -156,7 +156,7 @@ class TextToImageAsynchronous(TextToImage):
             print(f"[{ftime()}]-(TTI): Request sent to API for user request URQ-{requesting_uid}")
             print(f"[{ftime()}]-(TTI): Returning request response for user request URQ-{requesting_uid}")
             print(f"[{ftime()}]-(TTI): Showing raw data for request URQ-{requesting_uid}")
-            print(f"[{ftime()}]-(TTI): Returning request URQ-{requesting_uid}")
+            print(f"[{ftime()}]-(TTI): URQ-{requesting_uid}:\n{}")
             return [
                 request.status_code,
                 request.json(),
