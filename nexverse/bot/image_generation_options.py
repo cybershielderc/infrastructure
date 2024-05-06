afff9ac1bd44d73fa67678b7853602276978d133
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 22:02:22 +0300

    Commit H-3d815f204157448a

--- nexverse/bot/image_generation_options.py
+++ nexverse/bot/image_generation_options.py
@@ -6,5 +6,5 @@ from telegram import (
 
 def size_options() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup([
-        [InlineKeyboardButton('512x512', callback_data='size=[')]
+        [InlineKeyboardButton('512x512', callback_data='size=512x512')]
     ])
