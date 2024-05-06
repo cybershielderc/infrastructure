273f75a8a01fd15cf8ba2ab65d304234241b61a3
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 22:34:51 +0300

    Commit H-46ca4d9c0e655030

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -163,10 +163,10 @@ class NexVerse:
             else:
                 message = \
                     f"You have selected <strong>{context.user_data['selected_model_name']}</strong>\n\n" + \
-                    f"<strong>Sample Count </strong><code>{context.user_data['number_of_samples']}</code>\n" + \
+                    f"<strong>Sample Count </strong><code>{context.user_data['number_of_samples']}</code>\n\n" + \
                     f"<strong>Image Size</strong>" + \
-                    f"<code>{context.user_data['image_width']}x{context.user_data['image_height']}</code>\n" + \
-                    f"<strong>Inference Steps </strong><code>{inference_steps}</code>" + \
+                    f"<code>{context.user_data['image_width']}x{context.user_data['image_height']}</code>\n\n" + \
+                    f"<strong>Inference Steps </strong><code>{inference_steps}</code>\n\n" + \
                     f"the <strong>better</strong> the image!"
                 await update.callback_query.edit_message_text(
                     text=message,
