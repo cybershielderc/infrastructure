7af3508118924ae3c966c9f6560d8fd0911c62ab
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 22:20:58 +0300

    Commit H-e56adc790f898c72

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -19,7 +19,7 @@ from .start_menu import start_menu_markup
 from .image_models_menu import ai_models_image_menu
 from ..modelslab_api import MODEL, TextToImageAsynchronous
 from .image_generation_options import (
-
+    a
 )
 
 import re
