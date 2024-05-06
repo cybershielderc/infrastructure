39a7a6e7e4c813e44c376d5c471ccdaf55051f32
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 22:11:36 +0300

    Commit H-5a44f29d5d3231ce

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -128,6 +128,7 @@ class NexVerse:
                                                  message_id=context.user_data['reply_message_id'])
                 context.user_data['reply_message_id'] = None
         if not update.message:
+            if resp
             response_message = await update.callback_query.message.reply_photo(
                 photo=image[1],
                 caption=message,
