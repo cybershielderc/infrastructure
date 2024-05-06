47f1481bfbd45f22823b807be67e06a6282cdca8
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 22:20:02 +0300

    Commit H-36aab3ad16ce9673

--- nexverse/bot/image_generation_options.py
+++ nexverse/bot/image_generation_options.py
@@ -35,6 +35,7 @@ def inference_steps() -> InlineKeyboardMarkup:
 def numberof_samples() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup([
         [
-            InlineKeyboardButton('1', callback_data='samples=1')
+            InlineKeyboardButton('1', callback_data='samples=1'),
+            InlineKeyboardButton('1', callback_data='samples=1'),
         ]
     ])
