6736a9df1c8a4b79cdd612372978b960f8876444
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 21:30:44 +0300

    Commit H-2897df0e7b60556f

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -140,7 +140,7 @@ class NexVerse:
             )
             context.user_data['tti_response_message'] = response_message.message_id
         else:
-            response_message = await update.callback_query.message.reply_photo(
+            response_message = await update.message.reply_photo(
                 photo=image[1],
                 caption=message,
                 parse_mode='HTML',
