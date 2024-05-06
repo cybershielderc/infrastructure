280c887dec2089aa7b235bf6e5547629bfd2978e
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 21:39:25 +0300

    Commit H-22d43c775380cb78

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -253,6 +253,8 @@ class TextToImageAsynchronous(TextToImage):
                                 data=json.dumps({'key': self.api_key}),
                                 headers={'Content-Type': 'application/json'}
                             ).status_code
+                            print(
+                                f"[{ftime()}]-(TTI): Awaiting 1 seconds before requesting image for URQ-{requesting_uid}")
                             await asyncio.sleep(1)
                     print(f"[{ftime()}]-(TTI): Returning request URQ-{requesting_uid}\n" + \
                           f"[{ftime()}]-(TTI): URQ-{requesting_uid} Data: \n{response[1]}")
