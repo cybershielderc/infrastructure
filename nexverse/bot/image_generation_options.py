9b727933cfd4d220f454250910f816ef606d8e4e
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 22:04:42 +0300

    Commit H-652ded736a8f56a9

--- nexverse/bot/image_generation_options.py
+++ nexverse/bot/image_generation_options.py
@@ -15,6 +15,6 @@ def size_options() -> InlineKeyboardMarkup:
 def inference_steps() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup([
         [
-            InlineKeyboardButton('10')
+            InlineKeyboardButton('10', callback_data='infs=10')
         ]
     ])
