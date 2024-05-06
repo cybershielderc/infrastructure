4b119b5c46df85ebad3ef58e89de3d83823eb276
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 22:25:30 +0300

    Commit H-279f09fd4565e849

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -112,7 +112,7 @@ class NexVerse:
                 )
             else:
                 await update.callback_query.edit_message_text(
-                    text=f"You have selected <strong>{selection[1]}</strong>\n\n<strong>Sample Count </strong><code>{number_of_samples}</code>",
+                    text=f"You have selected <strong>{selection[1]}</strong>\n\n<strong>Sample Count </strong><code>{number_of_samples}</code>\nPlease select the size of the image",
                     parse_mode="HTML",
                     reply_markup=size_options()
                 )
