35de00b9868b5ecdb24a1bbac4f8f5b6a3d5a0ef
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 16:51:17 +0300

    Commit H-e22f8f757d1a2b90

--- nexverse/modelslab_api/__init__.py
+++ nexverse/modelslab_api/__init__.py
@@ -200,7 +200,8 @@ class TextToImageAsynchronous(TextToImage):
                               guidance_scale: float = 7.5,
                               safety_checker: bool = False
                               ):
-        print(f"[{ftime()}]-(TTI): request URQ-{requesting_uid} parameters:")
+        print(f"[{ftime()}]-(TTI): request URQ-{requesting_uid} parameters:"+\
+              )
         start_time = time.time()
         response = await self.get_image(
             model, requesting_uid, prompt, neg_prompt,
