d79dd52e080b147caa96caa4b7acef6d8af2537f
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-06 22:26:51 +0300

    Commit H-1bfe27b60d870f6f

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -120,6 +120,8 @@ class NexVerse:
         if query.data.startswith('samples='):
             number_of_samples = query.data.split("samples=")[1]
             if update.message is None:
+                message = \
+                
                 await update.callback_query.edit_message_text(
                     text=f"You have selected <strong>{selection[1]}</strong>\n\n<strong>Sample Count </strong><code>{number_of_samples}</code>\nPlease select the size of the image you would like the AI to use!",
                     parse_mode="HTML",
