32aac6e7504458222c8ad2b3ba63a08075910da6
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-07 12:11:45 +0300

    Commit H-0f30552029f8ae80

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -163,7 +163,7 @@ class NexVerse:
                         mnam=context.user_data['selected_model_name'],
                         samc=context.user_data['number_of_samples'],
                         imsi=context.user_data['image_height'] + "x" + context.user_data['image_width'],
-                        infs=
+                        infs=context.user_data['']
                     ),
                     parse_mode="HTML",
                     reply_markup=None
