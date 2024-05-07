38e7cd28c21a681dcf28612e41d03109699e914e
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-07 12:13:45 +0300

    Commit H-d503a4c1a27dd34d

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -308,7 +308,8 @@ class NexVerse:
                     await update.message.reply_to_message.edit_text(
                         text=self.lang['reply_neg_prompt'].format(
                             mnam=context.user_data['selected_model_name'],
-                            samc=context.user_data['']
+                            samc=context.user_data['number_of_samples'],
+                            
                         ),
                         parse_mode="HTML",
                         reply_markup=None
