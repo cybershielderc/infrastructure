315bb708253363c591a7a03574bdc1adb800c807
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-07 11:56:53 +0300

    Commit H-a01fb0368272125e

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -90,7 +90,7 @@ class NexVerse:
             selection = query.data.split("ai>img>model>selection#//")[1].split("#CNAME#")
             if update.message is None:
                 await update.callback_query.edit_message_text(
-                    text=f"You have selected <strong>{selection[1]}</strong>\nPlease select the amount of samples you would like the\nbot to generate!",
+                    text=self.lang['select_samples'].,
                     parse_mode="HTML",
                     reply_markup=numberof_samples()
                 )
