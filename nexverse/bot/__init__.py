0b6bf01c002de9c0b60fcc8c7860436429226938
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-07 12:17:52 +0300

    Commit H-53f3d044a0d75a06

--- nexverse/bot/__init__.py
+++ nexverse/bot/__init__.py
@@ -278,7 +278,7 @@ class NexVerse:
                         f"<strong>User Prompt</strong>\n" + context.user_data[
                             'pos_prompt'] + "\n\n<strong>User Negative Prompt</strong>\n" + user_input
                     await update.message.reply_to_message.edit_text(
-                        text=response_message,
+                        text=self.lang[''],
                         parse_mode="HTML",
                         reply_markup=None
                     )
