9793759a44e039dd4d123ebcac9814128519d056
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-08 16:23:47 +0300

    Commit H-4ec7704a7d202366

--- nexverse/bot/image_generation_options.py
+++ nexverse/bot/image_generation_options.py
@@ -20,28 +20,24 @@ def inference_steps() -> InlineKeyboardMarkup:
             InlineKeyboardButton('60', callback_data='infs=60'),
             InlineKeyboardButton('80', callback_data='infs=80')
         ],
-        [InlineKeyboardButton('One-time Payment of 500 $GEN to use!', callback_data='ignore_0xdead')],
         [
             InlineKeyboardButton('100', callback_data='infs=100'),
             InlineKeyboardButton('120', callback_data='infs=120'),
             InlineKeyboardButton('140', callback_data='infs=140'),
             InlineKeyboardButton('160', callback_data='infs=160')
         ],
-        [InlineKeyboardButton('One-time Payment of 1,000 $GEN to use!', callback_data='ignore_0xdead')],
         [
             InlineKeyboardButton('200', callback_data='infs=200'),
             InlineKeyboardButton('300', callback_data='infs=300'),
             InlineKeyboardButton('400', callback_data='infs=400'),
             InlineKeyboardButton('500', callback_data='infs=500')
         ],
-        [InlineKeyboardButton('One-time Payment of 2,000 $GEN to use!', callback_data='ignore_0xdead')],
         [
             InlineKeyboardButton('600', callback_data='infs=600'),
             InlineKeyboardButton('700', callback_data='infs=700'),
             InlineKeyboardButton('800', callback_data='infs=800'),
             InlineKeyboardButton('1,000', callback_data='infs=1000')
         ],
-        [InlineKeyboardButton('One-time Payment of 3,000 $GEN to use!', callback_data='ignore_0xdead')],
         [
             InlineKeyboardButton('1,500', callback_data='infs=1500'),
             InlineKeyboardButton('2,000', callback_data='infs=2000'),
