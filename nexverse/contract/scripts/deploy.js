8384142ab0c8bb2f00653a151eba06376bc2f59a
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-09 07:33:14 +0300

    Commit H-2ecc3ea509036150

--- nexverse/contract/scripts/deploy.js
+++ nexverse/contract/scripts/deploy.js
@@ -2,7 +2,7 @@ async function main() {
     const gentialToken = await ethers.getContractFactory("gentialToken");
  
     // Start deployment, returning a promise that resolves to a contract object
-    const _gent = await gentialToken.deploy();   
+    const _gent = await gentialToken.estimageGas.deploy();   
     console.log("Contract deployed to address:", _gent.address);
  }
  
