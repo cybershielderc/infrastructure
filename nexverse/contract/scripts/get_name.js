5c110534a9e16f13db4c29fd6b26f4b8ee96a62c
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-09 04:21:36 +0300

    Commit H-9886484b77597a56

--- nexverse/contract/scripts/get_name.js
+++ nexverse/contract/scripts/get_name.js
@@ -19,7 +19,7 @@ async function main() {
     console.log("The name is: " + name);
     console.log("The symbol is: " + symbol);
     console.log("The decimals is: " + decimals);
-    const gasLimit = GenTContract.estimateGas.reduceTax();
+    const gasLimit = GenTContract.estimateGas.reduceTax(25,25);
     console.log("The gasLimit for reduceTax is: " + gasLimit);
     //console.log(await GenTContract.reduceTax(25,25))
   }
