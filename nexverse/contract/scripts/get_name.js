eaac858b4d9dfd8f218d7ee1c00f650949ec8271
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-09 04:06:19 +0300

    Commit H-b6b86f891ac88fc1

--- nexverse/contract/scripts/get_name.js
+++ nexverse/contract/scripts/get_name.js
@@ -3,3 +3,5 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
 
 const contract = require("../artifacts/contracts/GenT.sol/GenT.json");
+
+console.log(JSON.stringify(contract.abi));
