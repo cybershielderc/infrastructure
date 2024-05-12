0a58c5a37c42032c1f278d21ef3c0d82a3a6b962
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-12 23:44:37 +0300

    Commit H-d77fb63a2996e2f7

--- auton_net/database/tables/create_portfolios.sql
+++ auton_net/database/tables/create_portfolios.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS `portfolio`
     `text2`             TEXT(256) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT 'Text Block #2',
     `skills_tgid`       INT(20)                                       DEFAULT '0' COMMENT 'Relationship ID to skills table',
     `contact_tgid`      INT(20)                                       DEFAULT '0' COMMENT 'Relationship',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`tg_id`)
 );
