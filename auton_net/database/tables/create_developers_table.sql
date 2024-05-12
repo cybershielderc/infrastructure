e170392cc331784b093155fc3b3343e0817076b4
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-12 12:16:03 +0300

    Commit H-02f06e40acd6ad29

--- auton_net/database/tables/create_developers_table.sql
+++ auton_net/database/tables/create_developers_table.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `developers`
     `isAnonymous`          INT(1)                                        DEFAULT '1',
     `nickname`             TEXT(50) CHARACTER SET utf8 COLLATE utf8_bin  DEFAULT 'Anonymous Developer',
     `connected_wallet`     TEXT(256) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '0xdead' COMMENT 'ETH 42-character Hexadecimal Address',
-    `completed_orders` INT(20)
+    `completed_orders` INT(20) DEFAULT
 
     PRIMARY KEY (`tg_id`)
 ) ENGINE = InnoDB;
