f085195af5ba1d6c27be1d70a072db95bd799455
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 01:31:12 +0300

    Commit H-414e117ef4ff053d

--- auton_net/bot/menus/__init__.py
+++ auton_net/bot/menus/__init__.py
@@ -2,9 +2,17 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 # Small Menus
+def start_menu() -> InlineKeyboardMarkup:
+    return InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("🛠️ Marketplace 🧾", callback_data="mp_1")],
+        ]
+    )
+
+
 def marketplace_panel() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup([
         [
-            
+
         ]
     ])
