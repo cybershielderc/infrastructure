66ff9036610e39596f8add25e2f41a1f0aa67234
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-12 11:56:06 +0300

    Commit H-f55a6e48f81832b0

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -15,3 +15,20 @@ from telegram.ext import (
 import os
 from urllib.request import urlretrieve
 from urllib.parse import urlparse
+
+
+class NexVerse:
+    def __init__(self, token: str, ai_image_api: TextToImageAsynchronous, lang_dict: dict):
+        self.token = token
+        self.ai_image_api = ai_image_api
+        self.lang = lang_dict
+        self.app = self._initialize_bot()
+
+    def _initialize_bot(self) -> ApplicationBuilder:
+        app: ApplicationBuilder = ApplicationBuilder().token(self.token).build()
+        app.add_handler(CallbackQueryHandler(self.start_menu, "main"))
+        app.add_handler(CallbackQueryHandler(self.image_ai_menu, "m1"))
+        app.add_handler(CallbackQueryHandler(self.button_input))
+        app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.text_input))
+        app.add_handler(CommandHandler("start", self.start_menu))
+        return app
