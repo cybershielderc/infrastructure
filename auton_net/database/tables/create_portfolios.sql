c173aa5fe7e5495d37b576b9113739a8a44cdec0
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-12 23:44:16 +0300

    Commit H-b4786582ea137de6

--- auton_net/database/tables/create_portfolios.sql
+++ auton_net/database/tables/create_portfolios.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `portfolio`
 (
-    `id`                INT(20) AUTO_INCREMENT COMMENT 'Used as an RID in developers table',
+    `tg_id`                INT(20) DEFAULT COMMENT 'Used as an RID in developers table',
     `portfolio_summary` TEXT(128) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT 'Developer Portfolio Summary',
     `text1`             TEXT(256) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT 'Text Block #1',
     `text2`             TEXT(256) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT 'Text Block #2',
