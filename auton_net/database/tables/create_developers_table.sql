4b1ab9422e8a61d12e6ed95965b2dc45a309f60a
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-12 12:15:27 +0300

    Commit H-2372c1b026c120ba

--- auton_net/database/tables/create_developers_table.sql
+++ auton_net/database/tables/create_developers_table.sql
@@ -1,13 +1,14 @@
 CREATE TABLE IF NOT EXISTS `developers`
 (
-    `tg_id`                INT(20)                                      DEFAULT '0',
-    `linked_portfolio_rid` INT(20)                                      DEFAULT '0',
-    `accepted_budget_min`  INT(20)                                      DEFAULT '100',
-    `accepted_budget_max`  INT(20)                                      DEFAULT '200',
-    `accepted_worktime`    INT(20)                                      DEFAULT '4' COMMENT 'In Days',
-    `rating`               FLOAT(20)                                    DEFAULT '1.0',
-    `isVerified`           INT(1)                                       DEFAULT '0',
-    `isAnonymous`          INT(1)                                       DEFAULT '1',
-    `nickname`             TEXT(50) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT 'Anonymous Developer',
+    `tg_id`                INT(20)                                       DEFAULT '0',
+    `linked_portfolio_rid` INT(20)                                       DEFAULT '0',
+    `accepted_budget_min`  INT(20)                                       DEFAULT '100',
+    `accepted_budget_max`  INT(20)                                       DEFAULT '200',
+    `accepted_worktime`    INT(20)                                       DEFAULT '4' COMMENT 'In Days',
+    `rating`               FLOAT(20)                                     DEFAULT '1.0',
+    `isVerified`           INT(1)                                        DEFAULT '0',
+    `isAnonymous`          INT(1)                                        DEFAULT '1',
+    `nickname`             TEXT(50) CHARACTER SET utf8 COLLATE utf8_bin  DEFAULT 'Anonymous Developer',
+    `connected_wallet`     TEXT(256) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT '0xdead' COMMENT 'ETH 42-character Hexadecimal Address',
     PRIMARY KEY (`tg_id`)
 ) ENGINE = InnoDB;
