6412c81cdcd5eaea260d499922d4ae9457c6dfae
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 01:27:44 +0300

    Commit H-9592e34b8c30930d

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -33,7 +33,7 @@ class AutonNET:
 
     def _initialize_bot(self) -> ApplicationBuilder:
         app: ApplicationBuilder = ApplicationBuilder().token(self.token).build()
-        app.add_handler(CallbackQueryHandler(self.image_ai_menu, "mp_1"))
+        app.add_handler(CallbackQueryHandler(self.mp_panel, "mp_1"))
 
         # app.add_handler(CallbackQueryHandler(self.start_menu, "main"))
         # app.add_handler(CallbackQueryHandler(self.image_ai_menu, "m1"))
