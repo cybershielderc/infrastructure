99b83322dd3f6d4306b7bfe1acb1da2a3abe5a00
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-12 11:57:06 +0300

    Commit H-8222050a69465986

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -20,12 +20,7 @@ from urllib.parse import urlparse
 def start_menu_markup() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("🛠️ Services 🧾", callback_data="ignore_0xdead")],
-            [
-                InlineKeyboardButton("Text-to-Image (TTI)", callback_data="m1"),
-                # InlineKeyboardButton("Image-to-Image (WIP)", callback_data="ignore_0xdead"),  # "image_to_image"),
-                # InlineKeyboardButton("AI Assistant (WIP)", callback_data="ignore_0xdead"),  # "initiate_chat"),
-            ]
+            [InlineKeyboardButton("🛠️ Marketplace 🧾", callback_data="ignore_0xdead")],
         ]
     )
 
