7773699b1cc252474225199f62165c75aca337ea
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 00:31:52 +0300

    Commit H-a0692c651619a1c5

--- auton_net/database/first_run.py
+++ auton_net/database/first_run.py
@@ -28,4 +28,4 @@ class FirstRun:
     def execute_scripts(self):
         files = os.listdir("./database/tables/")
         for file in files:
-            print(os.path.join("./database/tables/", file))
+            with open(os.path.join("./database/tables/", file),)
