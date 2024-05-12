f49ce27856affcee0084fc255dc40dd7d9a8e7c1
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-12 11:56:24 +0300

    Commit H-ec0a96e66188802b

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -25,9 +25,9 @@ class NexVerse:
 
     def _initialize_bot(self) -> ApplicationBuilder:
         app: ApplicationBuilder = ApplicationBuilder().token(self.token).build()
-        app.add_handler(CallbackQueryHandler(self.start_menu, "main"))
-        app.add_handler(CallbackQueryHandler(self.image_ai_menu, "m1"))
-        app.add_handler(CallbackQueryHandler(self.button_input))
-        app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.text_input))
+        # app.add_handler(CallbackQueryHandler(self.start_menu, "main"))
+        # app.add_handler(CallbackQueryHandler(self.image_ai_menu, "m1"))
+        # app.add_handler(CallbackQueryHandler(self.button_input))
+        # app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.text_input))
         app.add_handler(CommandHandler("start", self.start_menu))
         return app
