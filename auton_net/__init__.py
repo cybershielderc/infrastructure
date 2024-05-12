2fa2e9d4991e19710b40f3aeec241b528be1f6f6
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 00:20:19 +0300

    Commit H-a005a1385d89b423

--- auton_net/__init__.py
+++ auton_net/__init__.py
@@ -1,8 +1,10 @@
 from bot.main import run_app
 import json
+from database import FirstRun
 
 if __name__ == '__main__':
     with open("cfgs/bot.json", "r") as file:
         data = json.loads(file.read())
         file.close()
+
     run_app(data["keys"]["tg"], {})
