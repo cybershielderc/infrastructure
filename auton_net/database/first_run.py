b1e3f066aab761d30401d9305294a29517012050
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 00:02:12 +0300

    Commit H-ef1535e19067cae0

--- auton_net/database/first_run.py
+++ auton_net/database/first_run.py
@@ -1,5 +1,14 @@
 import mysql.connector
 
+
 class FirstRun:
     def __init__(self, data, credentials):
-        
+        self.data = data
+        self.credentials = credentials
+
+    def connect(self):
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="yourusername",
+            password="yourpassword"
+        )
