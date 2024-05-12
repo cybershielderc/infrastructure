36f0182e1fef23963ed63752625df644b18b23b6
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 01:35:04 +0300

    Commit H-a64f2b5a7f135f6c

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -58,7 +58,7 @@ class AutonNET:
         query = update.callback_query
         await query.answer()
         await query.edit_message_text(
-            text="",
+            text=query,
             reply_markup=marketplace_panel()
         )
 
