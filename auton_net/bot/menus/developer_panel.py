ab2dd8f6006e492bf7b6ee270e8f04e4b15608dd
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 02:41:00 +0300

    Commit H-bb2da5192496a1a7

--- auton_net/bot/menus/developer_panel.py
+++ auton_net/bot/menus/developer_panel.py
@@ -4,6 +4,6 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 def developer_panel_main() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         [
-            
+            []
         ]
     )
