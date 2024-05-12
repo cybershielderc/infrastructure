5d2aec000db93fb910fa5799da00f0b956ba3ec3
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 02:43:44 +0300

    Commit H-65d41858812438b3

--- auton_net/bot/menus/developer_panel.py
+++ auton_net/bot/menus/developer_panel.py
@@ -7,8 +7,8 @@ def developer_panel_main() -> InlineKeyboardMarkup:
             [InlineKeyboardButton("🛠️ Developer Panel 🧾", callback_data="ignore_0xdead")],
             [
                 InlineKeyboardButton("Stats", callback_data="mp_dev_stats"),
-                InlineKeyboardButton("Stats", callback_data="mp_dev_stats"),
-                InlineKeyboardButton("Stats", callback_data="mp_dev_stats"),
+                InlineKeyboardButton("Orders", callback_data="mp_dev_orders"),
+                InlineKeyboardButton("Awaiting Funds", callback_data="mp_dev_funds"),
             ]
         ]
     )
