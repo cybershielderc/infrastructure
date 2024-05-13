522a3d579c328d0c65ce656a282eed55683f949c
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 03:59:45 +0300

    Commit H-76e81ce37d51f163

--- auton_net/database/check_developer_status.py
+++ auton_net/database/check_developer_status.py
@@ -2,4 +2,5 @@ import mysql.connector
 import os
 
 class CheckDeveloperStatus:
-    def __init__(self, data: dict, credentials: dict)
+    def __init__(self, data: dict, credentials: dict):
+        
