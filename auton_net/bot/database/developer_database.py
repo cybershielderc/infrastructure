c64c5f12c31fc2f874956c23d5dcefdcc1b06468
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 05:02:16 +0300

    Commit H-be6bdd8ff18a97b7

--- auton_net/bot/database/developer_database.py
+++ auton_net/bot/database/developer_database.py
@@ -30,3 +30,22 @@ class CheckDeveloperStatus:
         except Exception as e:
             raise e
 class CreateDeveloperDatapoint:
+    @staticmethod
+    def check_developer_status(data: dict = None, credentials: dict = None, telegram_id: str = None) -> bool:
+        if data is None:
+            raise Exception('data is required')
+        if credentials is None:
+            raise Exception('credentials are required')
+        if telegram_id is None:
+            raise Exception('Telegram ID is required')
+        try:
+            connection = mysql.connector.connect(
+                host=data['host'],
+                user=credentials['username'],
+                password=credentials['password'],
+                database=data['database']
+            )
+        except mysql.connector.errors.DatabaseError as e:
+            raise e
+        try:
+            cursor = connection.cursor()
