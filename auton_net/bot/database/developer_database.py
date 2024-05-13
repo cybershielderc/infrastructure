7b535fac7e6defb89cf54cc108ed3a0c87d90c0e
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 05:23:26 +0300

    Commit H-3fb5e5aad1774b9a

--- auton_net/bot/database/developer_database.py
+++ auton_net/bot/database/developer_database.py
@@ -40,7 +40,8 @@ class CreateDeveloperDatapoint:
                          accepted_budget_max: int = None,
                          accepted_worktime_min: int = None,
                          accepted_worktime_max: int = None,
-                         isAnonymous: int = None
+                         isAnonymous: int = None,
+                         
                          ) -> bool:
         if data is None:
             raise Exception('data is required')
@@ -63,7 +64,7 @@ class CreateDeveloperDatapoint:
             INSERT INTO `developers`
             (`tg_id`, `linked_portfolio_rid`, `accepted_budget_min`, `accepted_budget_max`, `accepted_worktime_min`, `accepted_worktime_max`, `rating`, `isVerified`, `isAnonymous`, `nickname`, `connected_wallet`, `completed_orders`, `open_orders`, `rejected_orders`, `avg_completion_time`, `isAddressVerified`)
             VALUES
-            ({telegram_id}, -1, {accepted_budget_min}, {accepted_budget_max}, {accepted_worktime_min},{accepted_worktime_max}, 5.0, 0, 1, 'Anonymous Developer', '0xdead', 0, 0, 0, 1.5, 0);
+            ({telegram_id}, -1, {accepted_budget_min}, {accepted_budget_max}, {accepted_worktime_min},{accepted_worktime_max}, 5.0, 0, {isAnonymous}, 'Anonymous Developer', '0xdead', 0, 0, 0, 1.5, 0);
             """
         except Exception as e:
             raise e
