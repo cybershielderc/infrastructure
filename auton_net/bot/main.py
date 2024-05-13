f7c65c1a26adf2ec848ee4b5a0371d997c7300ef
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 04:27:20 +0300

    Commit H-77ce7f9cf9e85571

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -81,7 +81,7 @@ class AutonNET:
                 reply_markup=developer_panel_main()
             )
         else:
-            await query.edit_message_reply_markup(
+            await query.edit_message_text(
                 text="Sorry, but you have not been registered as a developer!\nWould you like to register?",
                 reply_markup=InlineKeyboardMarkup(
                     [
