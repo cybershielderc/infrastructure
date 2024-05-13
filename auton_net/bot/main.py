c189c6a41e54e1f1a9dc2e0fea60856fdcd4024f
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 05:01:14 +0300

    Commit H-88e900fe967a156a

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -63,14 +63,14 @@ class AutonNET:
                 reply_markup=start_menu() if not menu else menu,
                 photo=open("./bot/images/banners/banner.jpg", "rb"),
             )
-    
+
     async def mp_panel(self, update: Update, context: CallbackContext) -> None:
         query = update.callback_query
         await query.answer()
         await query.edit_message_reply_markup(
             reply_markup=marketplace_panel(),
         )
-
+        
     async def mp_dev_panel(self, update: Update, context: CallbackContext) -> None:
         query = update.callback_query
         await query.answer()
