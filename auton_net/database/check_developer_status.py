7cc33e94b2643bad50fdc1649155fdad58ee7ca8
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 04:04:42 +0300

    Commit H-bd81e6a6ae5e8e29

--- auton_net/database/check_developer_status.py
+++ auton_net/database/check_developer_status.py
@@ -5,4 +5,5 @@ import os
 class CheckDeveloperStatus:
     @staticmethod
     def check_developer_status(data: dict, credentials: dict, telegram_id: str = None) -> bool:
-        
+        if telegram_id is None:
+            raise
