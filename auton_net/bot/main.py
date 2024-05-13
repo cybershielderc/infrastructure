68a029a1e1729eaffb155d02a965f1d5b6722758
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 06:42:49 +0300

    Commit H-7f01bf0d99285b11

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -103,7 +103,9 @@ class AutonNET:
                 selection = True if query.data.split("dev_reg#anon>>")[1] is 'yes' else False
                 await query.answer()
                 context.user_data['dev_reg#anon'] = selection
-                await query.edit_message_caption()
+                await query.edit_message_caption(
+                    caption=''
+                )
         await query.answer()
 
     def run(self):
