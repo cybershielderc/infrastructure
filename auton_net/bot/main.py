8e36bcd5d19de5d21fc06748a8318be968749f12
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 06:40:42 +0300

    Commit H-7b5002818f5d3829

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -92,6 +92,7 @@ class AutonNET:
 
     async def registeration_input(self, update: Update, context: CallbackContext) -> None:
         query = update.callback_query
+        if query.data.startswith("ignore_0xdead"): await query.answer()
         if query.data.startswith("mp_dev_start_registration"):
             await query.answer()
             context.user_data['devRegistration'] = True
