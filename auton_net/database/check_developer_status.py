33997771fd30d51a6d941d96eb155776b0532876
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 04:05:12 +0300

    Commit H-713cdf1e68e0a65f

--- auton_net/database/check_developer_status.py
+++ auton_net/database/check_developer_status.py
@@ -5,5 +5,7 @@ import os
 class CheckDeveloperStatus:
     @staticmethod
     def check_developer_status(data: dict = None, credentials: dict = None, telegram_id: str = None) -> bool:
+        if data is None:
+            raise Exception("")
         if telegram_id is None:
             raise Exception('Telegram ID is required')
