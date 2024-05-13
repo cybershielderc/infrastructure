6649fc362ac2424b32773fb1092d281e9cf9e265
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-13 04:04:47 +0300

    Commit H-84add3917777d175

--- auton_net/database/check_developer_status.py
+++ auton_net/database/check_developer_status.py
@@ -7,3 +7,4 @@ class CheckDeveloperStatus:
     def check_developer_status(data: dict, credentials: dict, telegram_id: str = None) -> bool:
         if telegram_id is None:
             raise Exception('Telegram ID is required')
+        
