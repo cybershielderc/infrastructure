fc124336a3b2191ffb5032c98f50ebfe173ca3fc
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:54:19 +0300

    Commit H-a3b397b60c63cf41

--- auton_net/bot/actions/register/developer/register_text_input.py
+++ auton_net/bot/actions/register/developer/register_text_input.py
@@ -1,3 +1,4 @@
+from telegram.ext import CallbackContext
 
 
-async def handle_seller_text_input(update: Update, context: C)
+async def handle_seller_text_input(update: Update, context: CallbackContext) -> None:
