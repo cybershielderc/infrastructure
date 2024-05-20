1de4afc44ddc919703accf3852e212faccaf0edf
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:41:01 +0300

    Commit H-054cc3d9333a31cc

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -128,9 +128,9 @@ class AutonNET:
         query = update.callback_query
         if query.data.startswith("ignore_0xdead"): await query.answer()
         if 'devRegistration' in context.user_data:
-            await seller_registration(update, context)
+            await seller_register_logic(update, context)
         if 'clientRegistration' in context.user_data:
-            await seller_registration(update, context)
+            await seller_register_logic(update, context)
         await query.answer()
 
     async def text_input(self, update: Update, context: CallbackContext):
