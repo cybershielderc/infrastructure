a58c19b54ce9d3ef83dd9d0cd73184f5e3501ba7
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 21:57:18 +0300

    Commit H-10171511570f84d7

--- auton_net/bot/database/developer_database.py
+++ auton_net/bot/database/developer_database.py
@@ -141,6 +141,7 @@ class GetDeveloperInformation:
             cursor.execute(
                 f"""SELECT * FROM developers WHERE tg_id={telegram_id};"""
             )
+            result = cursor.fetchone()
         except Exception as e:
             return [e]
         return [None]
