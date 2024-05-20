3ebe4d3d9bc8f76e8ac980b1a2b59d7984e6bde3
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:49:11 +0300

    Commit H-ca365e91d5cbc2e8

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -342,7 +342,7 @@ def run_app(token: str, bot_data: dict, lang_dict: dict) -> ApplicationBuilder:
 
 
 if __name__ == '__main__':
-    with open("cfgs/bot.json", "r") as file:
+    with open("./cfgs/bot.json", "r") as file:
         data = json.loads(file.read())
         file.close()
     f_run = FirstRun(data['database'], data['database']['credentials'])
