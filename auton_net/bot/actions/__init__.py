2ac88ad78eed841a0fa08526534572874dc76200
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:40:49 +0300

    Commit H-6e39c1222847a9d1

--- auton_net/bot/actions/__init__.py
+++ auton_net/bot/actions/__init__.py
@@ -1,1 +1,1 @@
-from auton_net.bot.actions.register.developer.register_logic import register_logic
+from auton_net.bot.actions.register.developer.register_logic import register_logic as seller_register_logic
