de7608132cffde8970ede4d0f23ca407760167d1
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-21 01:21:37 +0300

    Commit H-30bb598981d3c3df

--- auton_net/bot/actions/register/developer/register_logic.py
+++ auton_net/bot/actions/register/developer/register_logic.py
@@ -508,7 +508,7 @@ async def register_wallet_address(update: Update, context: CallbackContext) -> N
                     f"\n\nHere's some statistics for you to think about while we re-direct you\n" + \
                     f"to the seller panel we created for you!\n\n" + \
                     f"Your registration took roughly <strong>{datetime.now() - st_time} seconds</strong>!\n" + \
-                    f"You are now a seller amongst our large network of sellers!"
+                    f"You are now a <strong>seller</strong> amongst our large network of sellers!"
             )
 
 
