163434a4fb855715d4f8130f3046f5d11b6ccc4b
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:36:42 +0300

    Commit H-25b4d8aa8172239a

--- auton_net/bot/database/conversation_database.py
+++ auton_net/bot/database/conversation_database.py
@@ -7,4 +7,4 @@ class RetrieveAllConversations:
     @staticmethod
     def get_all_conversations(cls, data: dict = None):
         if not data: raise Exception("No database datat provided")
-        query = f'''SELECT conversation.initiator_id, conversation.participant'''
+        query = f'''SELECT conversation.initiator_id, conversation.participant_id, '''
