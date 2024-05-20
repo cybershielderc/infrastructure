a6ebee9af4f39940abb401ad8dcb027a2c20f21b
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-21 01:27:02 +0300

    Commit H-cc45689253a7264e

--- auton_net/bot/actions/register/developer/register_logic.py
+++ auton_net/bot/actions/register/developer/register_logic.py
@@ -514,8 +514,7 @@ async def register_wallet_address(update: Update, context: CallbackContext) -> N
                 parse_mode="HTML"
             )
             await asyncio.sleep(5)
-            await message.delete()
-            await
+            await context.bot_data["developer_panel"]()
 
 
 async def register_logic(update: Update, context: CallbackContext) -> None:
