7d63ae840ac00c54ea56037cd220c791d6dabc61
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:49:44 +0300

    Commit H-47e99e538cf52b81

---
+++ auton_net/bot/__init__.py
@@ -0,0 +1,16 @@
+from .main import run_app
+
+if __name__ == '__main__':
+    with open("./cfgs/bot.json", "r") as file:
+        data = json.loads(file.read())
+        file.close()
+    f_run = FirstRun(data['database'], data['database']['credentials'])
+    # Connect to First Run Model
+    f_run.connect()
+    # Execute Table Creation Scripts
+    f_run.execute_scripts()
+    # Run APP
+    try:
+        run_app(data["keys"]["tg"], data, {})
+    except KeyboardInterrupt as e:
+        exit(-1)
