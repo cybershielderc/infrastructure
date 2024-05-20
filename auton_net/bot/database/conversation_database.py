1b872056416596991096ee2032d60f3fb5638628
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:33:02 +0300

    Commit H-6448db68af861005

--- auton_net/bot/database/conversation_database.py
+++ auton_net/bot/database/conversation_database.py
@@ -4,4 +4,5 @@ import uuid
 
 class RetrieveAllConversations:
     @staticmethod
-    def get_all_conversations(cls, ):
+    def get_all_conversations(cls, data: dict = None):
+        if not data: 
