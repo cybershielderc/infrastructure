1ae0b915ffe2568fc791211f87fc24b30ae347d6
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 22:52:33 +0300

    Commit H-cd57ece8a3e3a0c9

--- auton_net/bot/database/developer_database.py
+++ auton_net/bot/database/developer_database.py
@@ -101,7 +101,7 @@ class GetDeveloperInformation:
     @staticmethod
     def get_developer_information(
             host: str = None, database: str = None, username: str = None, password: str = None, telegram_id: int = None
-    ) -> list:
+    ) -> list[]:
         """
         Gets developer information and returns it as a list object formatting in such a way
         @return [
