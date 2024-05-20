00385d392b4561c7d2ad438a56ab739cf4a62e77
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 21:46:06 +0300

    Commit H-8748d0f5cd7cd94f

--- auton_net/bot/actions/register/developer/register_logic.py
+++ auton_net/bot/actions/register/developer/register_logic.py
@@ -498,7 +498,7 @@ async def register_wallet_address(update: Update, context: CallbackContext) -> N
             raise e
         # After account creation edit the message that the account was created successfully
         if not message:
-            raise Exception("")
+            raise Exception("Account creation message ")
 
 
 async def register_logic(update: Update, context: CallbackContext) -> None:
