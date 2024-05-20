23e2752216c696fae0fb34cdc4762377003db57c
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:50:55 +0300

    Commit H-8a5a4e4266fc99c2

--- auton_net/__init__.py
+++ auton_net/__init__.py
@@ -1,6 +1,6 @@
-from bot.main import run_app
+from auton_net.bot.main import run_app
 import json
-from bot.database import (
+from auton_net.bot.database import (
     FirstRun,
     CheckDeveloperStatus
 )
