7bf2c3dc9fc4a205e4cf6882ce8000d7e3365868
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-20 19:33:13 +0300

    Commit H-e4aee526dbe0eea2

--- auton_net/bot/database/conversation_database.py
+++ auton_net/bot/database/conversation_database.py
@@ -6,4 +6,4 @@ import uuid
 class RetrieveAllConversations:
     @staticmethod
     def get_all_conversations(cls, data: dict = None):
-        if not data: raise Exception("")
+        if not data: raise Exception("No database datat provided")
