d05d93988989cb6d72b1a9a878d6f30310b0c12b
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-21 03:55:27 +0300

    Commit H-bde3c2fe159fc598

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -129,6 +129,7 @@ class AutonNET:
             host=context.bot_data['database_host'],
             database=context.bot_data['database_database'],
             username=context.bot_data['database_user'],
+            password=
         )
             await query.edit_message_reply_markup(reply_markup=developer_panel_main())
         else:
