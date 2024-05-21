96c9992d524befa2faa41ef5e6e20b2c439e14ba
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-21 03:54:32 +0300

    Commit H-b2698d8c3313f193

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -125,7 +125,7 @@ class AutonNET:
         query = update.callback_query
         await query.answer()
         # Check if user is a developer
-        seller_stats =
+        seller_stats = GetDeveloperInformation.
             await query.edit_message_reply_markup(reply_markup=developer_panel_main())
         else:
             await query.edit_message_caption(
