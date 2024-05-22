88d92349d4e347adc5e91969033c757a4ca4843b
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-23 01:51:32 +0300

    Commit H-430acf946c1ff687

---
+++ auton_net/contracts/hardhat.config.js
@@ -0,0 +1,24 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+//require("@nomicfoundation/hardhat-verify");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.21",
+   defaultNetwork: "sepolia",
+   networks: {
+      hardhat: {},
+      sepolia: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      },
+   },
+   etherscan: {
+    apiKey: "5WQXZ6HE9W9VEKVPM1VCSZPIIED69D7MBD"
+  }
+}
---
+++ auton_net/contracts/scripts/get_name.js
@@ -0,0 +1,26 @@
+const API_URL = process.env.API_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+
+const contract = require("../artifacts/contracts/GenT.sol/GenT.json");
+
+const ethers = require('ethers');
+
+const alchemyProvider = new ethers.providers.JsonRpcProvider(API_URL);
+
+const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
+
+const GenTContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main() {
+    const name = await GenTContract.name();
+    const symbol = await GenTContract.symbol();
+    const decimals = await GenTContract.decimals();
+    console.log("The name is: " + name);
+    console.log("The symbol is: " + symbol);
+    console.log("The decimals is: " + decimals);
+    const gasLimit = await GenTContract.reduceTax(30,30);
+    console.log("The gasLimit for reduceTax is: " + gasLimit);
+    //console.log(await GenTContract.reduceTax(25,25))
+  }
+  main();
