3fd4d477a462876296fa89ee3c5b695c7607817f
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-23 02:28:56 +0300

    Commit H-c34d27a5474b5dab

--- auton_net/contracts/scripts/get_name.js
+++ auton_net/contracts/scripts/get_name.js
@@ -13,15 +13,7 @@ const signer = new ethers.Wallet(PRIVATE_KEY, alchemyProvider);
 const GenTContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
 
 async function main() {
-    const name = await GenTContract.name();
-    const symbol = await GenTContract.symbol();
-    const decimals = await GenTContract.decimals();
-    console.log("The name is: " + name);
-    console.log("The symbol is: " + symbol);
-    console.log("The decimals is: " + decimals);
-    //const gasLimit = await GenTContract.console.log("The gasLimit for reduceTax is: " + gasLimit);
-    //console.log(await GenTContract.tradeTimiTrumpet())
-    //console.log(await GenTContract.reduceTax(50, 50));
+    
 }
 
 main();
