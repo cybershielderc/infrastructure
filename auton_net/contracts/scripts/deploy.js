3d7d4e35c764076fa6f28fbc4779df2a014105a6
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-23 01:55:40 +0300

    Commit H-ae8bc5409f0d10c2

--- auton_net/contracts/scripts/deploy.js
+++ auton_net/contracts/scripts/deploy.js
@@ -1,22 +1,22 @@
 async function main() {
     const [deployer] = await ethers.getSigners();
-    const gentialToken = await ethers.getContractFactory("AutonNet");
-  
-    const accountBalance = await deployer.provider.getBalance(deployer.address); 
+    const gentialToken = await ethers.getContractFactory("");
+
+    const accountBalance = await deployer.provider.getBalance(deployer.address);
     console.log(
-      "Deploying the contracts with the account:",
-      await deployer.getAddress()
+        "Deploying the contracts with the account:",
+        await deployer.getAddress()
     );
 
     console.log("Account balance:", accountBalance.toString());
     // Start deployment, returning a promise that resolves to a contract object
     const _gent = await gentialToken.deploy()
     console.log("Contract deployed to address:", _gent.address);
- }
- 
- main()
-   .then(() => process.exit(0))
-   .catch(error => {
-     console.error(error);
-     process.exit(1);
-   });
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
