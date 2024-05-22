cce61f9fc41a6713c81fc64cf66b27a92cf0c258
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-23 01:35:49 +0300

    Commit H-a8f321971c3c34f0

--- auton_net/bot/database/conversation_database.py
+++ auton_net/bot/database/conversation_database.py
@@ -7,7 +7,8 @@ class RetrieveAllConversations:
     @staticmethod
     def get_all_conversations(data: dict = None):
         """
-        Retrieves all holding conversations that are currently holding and are not paused!
+        Retrieves all holding conversations that are currently holding and are not paused
+        returns a list of tuples that store the initiator of the conversation and pa
         """
         if not data: raise Exception("No database datat provided")
         query = f'''SELECT initiator_id, participant_id FROM conversation WHERE isHolding = 1;'''
