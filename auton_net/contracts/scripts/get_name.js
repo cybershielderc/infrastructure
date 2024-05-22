33ba939e25380f33484cd7edb469caea23b30589
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-23 02:39:43 +0300

    Commit H-51fb1dbdabe53ed5

--- auton_net/contracts/scripts/get_name.js
+++ auton_net/contracts/scripts/get_name.js
@@ -15,11 +15,11 @@ const GenTContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer)
 async function main() {
     const taxWallet = '0x7ce00714d01e96e8d6fd3f0ea149d1fce5f027e9';
     console.log(await GenTContract.createPair());
-    console.log(await GenTContract.setTaxWallet(taxWallet));
-    const maxTaxSawp = ethers.utils.parseEther('5000000');
-    console.log(await GenTContract.setMaxTaxSwap(maxTaxSawp));
-    console.log(await GenTContract.removeLimits());
-    console.log(await GenTContract.reduceTax(5, 5));
+    //console.log(await GenTContract.setTaxWallet(taxWallet));
+    //const maxTaxSawp = ethers.utils.parseEther('5000000');
+    //console.log(await GenTContract.setMaxTaxSwap(maxTaxSawp));
+    //console.log(await GenTContract.removeLimits());
+    console.log(await GenTContract.reduceTax(50, 50));
 }
 
 main();
