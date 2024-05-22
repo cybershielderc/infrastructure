1d21b8eee4b5cf41513bf4de0b314b9ee9fb99ac
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-23 01:56:00 +0300

    Commit H-8f1f36d524584aa5

--- auton_net/contracts/scripts/deploy.js
+++ auton_net/contracts/scripts/deploy.js
@@ -1,6 +1,6 @@
 async function main() {
     const [deployer] = await ethers.getSigners();
-    const gentialToken = await ethers.getContractFactory("Tit");
+    const gentialToken = await ethers.getContractFactory("TimiTrumpet");
 
     const accountBalance = await deployer.provider.getBalance(deployer.address);
     console.log(
