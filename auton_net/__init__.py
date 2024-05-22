a97465635273c4baf86b054c64e524abdb5d23b5
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-23 01:25:19 +0300

    Commit H-90ff89001273b34f

--- auton_net/__init__.py
+++ auton_net/__init__.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     # Execute Table Creation Scripts
     f_run.execute_scripts()
     # Test retrieval scripts
-    
+    print("Attempting to retrieve all MFPC conversations...")
     # Run APP
     try:
         run_app(data["keys"]["tg"], data, {})
