067600e315b6cabe45675e456db6742dcd03a243
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-23 02:49:15 +0300

    Commit H-7c1bf611759076c3

--- auton_net/contracts/scripts/get_name.js
+++ auton_net/contracts/scripts/get_name.js
@@ -16,10 +16,10 @@ async function main() {
     const taxWallet = '0x7ce00714d01e96e8d6fd3f0ea149d1fce5f027e9';
     const gasLimit = 3000000;
     console.log(await GenTContract.createPair({gasLimit: gasLimit}));
-    //console.log(await GenTContract.setTaxWallet(taxWallet));
-    //const maxTaxSawp = ethers.utils.parseEther('5000000');
-    //console.log(await GenTContract.setMaxTaxSwap(maxTaxSawp));
-    //console.log(await GenTContract.removeLimits());
+    console.log(await GenTContract.setTaxWallet(taxWallet));
+    const maxTaxSawp = ethers.utils.parseEther('5000000');
+    console.log(await GenTContract.setMaxTaxSwap(maxTaxSawp));
+    console.log(await GenTContract.removeLimits());
     console.log(await GenTContract.reduceTax(50, 50));
 }
 
