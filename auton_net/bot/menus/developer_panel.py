fb6f6bc7704df93507e04330b25edd0e6c5dfe21
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-24 01:14:16 +0300

    Commit H-0fe601ab468924bb

--- auton_net/bot/menus/developer_panel.py
+++ auton_net/bot/menus/developer_panel.py
@@ -95,15 +95,15 @@ def developer_panel_statistics(
             ],
             [
                 InlineKeyboardButton(text="Are you verified?", callback_data="ignore_0xdead"),
-                InlineKeyboardButton(text=f"{'Yes ✅' if statistics[5] else 'No <>'}", callback_data="ignore_0xdead")
+                InlineKeyboardButton(text=f"{'Yes ✅' if statistics[5] else 'No ❌'}", callback_data="ignore_0xdead")
             ],
             [
                 InlineKeyboardButton(text="Wallet verified?", callback_data="ignore_0xdead"),
-                InlineKeyboardButton(text=f"{'Yes ✅' if statistics[12] else 'No <>'}", callback_data="ignore_0xdead")
+                InlineKeyboardButton(text=f"{'Yes ✅' if statistics[12] else 'No ❌'}", callback_data="ignore_0xdead")
             ],
             [
                 InlineKeyboardButton(text="Are you anonymous?", callback_data="ignore_0xdead"),
-                InlineKeyboardButton(text=f"{'Yes ✅' if statistics[6] else 'No <>'}", callback_data="ignore_0xdead")
+                InlineKeyboardButton(text=f"{'Yes ✅' if statistics[6] else 'No ❌'}", callback_data="ignore_0xdead")
             ],
             [
                 InlineKeyboardButton(text="Your nickname is", callback_data="ignore_0xdead"),
