e50901b90c5cd4a410c17a689eae9feaa9fb2275
Tal A. Baskin (~redstonedlife) <talbaskin.business@gmail.com>
2024-05-24 01:01:41 +0300

    Commit H-7e493f8ddca59058

--- auton_net/bot/main.py
+++ auton_net/bot/main.py
@@ -135,7 +135,7 @@ class AutonNET:
             telegram_id=update.effective_user.id
         )
         if seller_stats[0] is True:
-            await query.edit_message_reply_markup(reply_markup=developer_panel_main())
+            await query.edit_message_reply_markup(reply_markup=de())
         # else:
         #    await query.edit_message_caption(
         #        caption="Sorry, but you have not been registered as a developer!\nWould you like to register?",
